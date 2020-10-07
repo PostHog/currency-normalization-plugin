34602bce3d0963b1ab26e2e2789e336b687a0065
Marius Andra <marius.andra@gmail.com>
2020-10-08 01:43:18 +0200

    currency normalization works

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
-from abc import ABC, abstractmethod
-from openexchangerates import OpenExchangeRatesClient
-from datetime import datetime, timedelta
-
+import coinoxr
 import os
 
+from posthog.plugins import PluginBaseClass, PosthogEvent
+from datetime import datetime, timedelta
+
 openexchangerates_api_key = os.environ.get('OPENEXCHANGERATES_API_KEY', None)
+normalized_currency = os.environ.get("CNP_NORMALIZED_CURRENCY", None)
+
 amount_property = os.environ.get("CNP_AMOUNT_PROPERTY", "amount")
 currency_property = os.environ.get("CNP_CURRENCY_PROPERTY", "currency")
 normalized_amount_property = os.environ.get("CNP_NORMALIZED_AMOUNT_PROPERTY", "normalized_amount")
 normalized_currency_property = os.environ.get("CNP_NORMALIZED_CURRENCY_PROPERTY", "normalized_currency")
-normalized_currency = os.environ.get("CNP_NORMALIZED_CURRENCY", None)
 
 will_normalize = True
 
@@ -22,44 +23,41 @@ if not openexchangerates_api_key:
     print("🔻 Running posthog-maxmind-plugin without OPENEXCHANGERATES_API_KEY")
     print("🔺 No amounts will be normalized!")
     will_normalize = False
+else:
+    coinoxr.app_id = openexchangerates_api_key
 
 
-class Plugin(ABC):
-    @abstractmethod
-    def process_event(self, event):
-        pass
-
-    @abstractmethod
-    def process_person(self, event):
-        pass
-
-    @abstractmethod
-    def process_identify(self, event):
-        pass
+def fetch_rates():
+    try:
+        return coinoxr.Latest().get().body['rates']
+    except:
+        return None
 
 
-class CurrencyNormalizationPlugin(Plugin):
-    def __init__(self):
-        self.client = None
-        self.currency_rates = None
-        self.currency_rates_fetched_at = None
+currency_rates = None
+currency_rates_fetched_at = None
+if will_normalize:
+    currency_rates = fetch_rates()
+    currency_rates_fetched_at = datetime.now()
+
+
+class CurrencyNormalizationPlugin(PluginBaseClass):
+    def process_event(self, event: PosthogEvent):
         if will_normalize:
-            self.client = OpenExchangeRatesClient(openexchangerates_api_key)
-            self.currency_rates = self.client.latest()['rates']
-            self.currency_rates_fetched_at = datetime.now()
+            global currency_rates
+            global currency_rates_fetched_at
 
-    def process_event(self, event):
-        if will_normalize:
             # last fetched a day ago
-            if not self.currency_rates or self.currency_rates_fetched_at < datetime.now() - timedelta(days=1):
-                self.currency_rates = self.client.latest()['rates']
-                self.currency_rates_fetched_at = datetime.now()
+            if not currency_rates or currency_rates_fetched_at < datetime.now() - timedelta(days=1):
+                currency_rates = fetch_rates()
+                currency_rates_fetched_at = datetime.now()
 
-            amount = event.properties.get(amount_property, None)
-            currency = event.properties.get(currency_property, None)
-            if currency and self.currency_rates[currency] and self.currency_rates[normalized_currency]:
-                normalized_amount = ((amount or 0) * self.currency_rates[normalized_currency] / self.currency_rates[currency]).round(round)
-                event.properties[normalized_amount_property] = normalized_amount
-                event.properties[normalized_currency_property] = normalized_currency
+            if currency_rates:
+                amount = event.properties.get(amount_property, 0)
+                currency = event.properties.get(currency_property, None)
+                if currency and currency_rates[currency] and currency_rates[normalized_currency]:
+                    normalized_amount = round(amount * currency_rates[normalized_currency] / currency_rates[currency], 4)
+                    event.properties[normalized_amount_property] = normalized_amount
+                    event.properties[normalized_currency_property] = normalized_currency
 
         return event
