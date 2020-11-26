8f640324f009913dafd91b4173d5213bf79d91c6
Marius Andra <marius.andra@gmail.com>
2020-11-26 13:52:01 +0100

    remove lib.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-async function setupTeam({ config, cache }) {
+async function setupPlugin({ config, cache }) {
     const apiKey = config['openExchangeRatesApiKey'] || null
 
     if (apiKey) {
@@ -41,4 +41,34 @@ async function processEvent(event, { config, cache }) {
     }
 
     return event
+}
+
+// lib functions
+
+async function fetchRatesIfNeeded(config, cache) {
+    const currencyRatesFetchedAt = await cache.get('currency_rates_fetched_at')
+    if (!currencyRatesFetchedAt || currencyRatesFetchedAt < new Date().getTime() - 86400 * 1000) { // 24h
+        await fetchRates(config, cache)
+    }
+}
+
+async function fetchRates(config, cache) {
+    try {
+        const url = `https://openexchangerates.org/api/latest.json?app_id=${config['openExchangeRatesApiKey']}`
+        const response = await fetch(url)
+        const json = await response.json()
+
+        if (json && json['rates']) {
+            cache.set('currency_rates', json['rates'])
+            cache.set('currency_rates_fetched_at', new Date().getTime())
+        } else {
+            throw new Error('Error fetching currency rates!')
+        }
+    } catch (e) {
+        throw new Error('Error fetching currency rates!')
+    }
+}
+
+function roundToDigits(number, digits) {
+    return Math.round(number * Math.pow(10, digits)) / Math.pow(10, digits)
 }
